e45bcf2c3297b85987ab9dc93b99e4d9fe7ce67b
Gustavo Latini <latinidev@gmail.com>
2025-08-13 16:53:25 -0300

    actualizacion

--- pages/[merchantSlug]/index.js
+++ pages/[merchantSlug]/index.js
@@ -1,14 +1,20 @@
+import { supabase } from "../../lib/supabaseClient";
+
 export async function getServerSideProps({ params }) {
   const { data: merchant } = await supabase
-    .from('merchants')
-    .select('*')
-    .eq('slug', params.merchantSlug)
+    .from("merchants")
+    .select("*")
+    .eq("slug", params.merchantSlug)
     .single();
 
+    
   return { props: { merchant } };
 }
 
 export default function MerchantPage({ merchant }) {
+    if (!merchant) {
+    return <div>No se encontró el comercio.</div>;
+  }
   return (
     <div>
       <h1>{merchant.name}</h1>
